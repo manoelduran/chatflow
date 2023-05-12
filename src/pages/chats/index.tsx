3da3b70b8ba8b21a277c425e8bea5719de6e73b2
Manoel Duran <manoel.duran@hotmail.com>
2023-05-12 00:11:49 -0300

    refact(Chats): re created chats list

--- src/pages/chats/index.tsx
+++ src/pages/chats/index.tsx
@@ -1,51 +1,59 @@
 import AnimatedButton from '@/src/components/AnimatedButton';
+import AnimatedCard from '@/src/components/AnimatedCard';
+import { useAuth } from '@/src/contexts/auth';
 import { useChat } from '@/src/contexts/chat';
 import { useRouter } from 'next/router';
 import React, { useCallback, useMemo } from 'react';
 
 const Chats: React.FC = () => {
-//  const [chats, setChats] = useState<string[]>([]);
+  //  const [chats, setChats] = useState<string[]>([]);
+
   const router = useRouter();
-  const {chats, joinChat} = useChat()
-  const handleJoinChat = useCallback(async (chat_id: string | undefined) => {
-      if(chat_id) {
-        await joinChat({chat_id})
-        console.log(`Joining ${chat_id}`);
-      }
+  const { chats, joinChat, chatList } = useChat()
+  const { user } = useAuth();
+  const handleEnterChat = useCallback( (chat_id: string | undefined) => {
+    if (chat_id) {
+      router.push(`/chat/${chat_id}`);
+    }
   }, []);
+  const handleJoinChat = useCallback(async (chat_id: string | undefined) => {
+    if (chat_id) {
+      await joinChat({ chat_id })
+      console.log(`Joining ${chat_id}`);
+    }
+  }, []);
+  console.log('USER', user)
+  console.log('CHATS', chats)
   const list = useMemo(() => {
-    if(!chats) {
+    if (!chats) {
       return []
     }
     return chats
   }, [chats])
   return (
-    <div className="w-full h-screen px-10 py-10 flex flex-col items-center">
+    <div className="w-full overflow-y-auto max-h-screen px-10 py-10 flex flex-col items-center">
       <div className="w-full h-100 flex items-start justify-between">
-      <h1 className="text-2xl font-bold mb-6">ChatFlow</h1>
-      <AnimatedButton type="button" style="bg-indigo-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" path='/create-chat' text=' Create Chat' />
+        <h1 className="text-2xl font-bold mb-6">ChatFlow</h1>
+        <AnimatedButton type="button" style="bg-indigo-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" path='/create-chat' text=' Create Chat' />
       </div>
       <div className="w-full h-100 flex items-start justify-between">
-      <span className="text-2xl font-bold mb-6">Want to talk with someone? Just create your chat and enjoy!</span>
+        <span className="text-2xl font-bold mb-6">Want to talk with someone? Just create your chat and enjoy!</span>
       </div>
-      <div className=" rounded px-8 py-6">
-        <h2 className="text-2xl font-bold mb-6">Your Chats</h2>
+      <div className=" rounded  py-6">
+        <h2 className="text-2xl font-bold mb-6">Our Chats</h2>
         {list ? (
-          <ul className="space-y-4">
+          <div className="grid gap-20 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
             {list?.map((chat, index) => (
-              <li key={index}>
-                <div className="flex items-center">
-                  <span className="mr-2">{chat.name}</span>
-                  <button
-                    onClick={() => handleJoinChat(chat.id)}
-                    className="bg-indigo-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-                  >
-                    Join Chat
-                  </button>
-                </div>
-              </li>
+                  <AnimatedCard
+                key={chat.id}
+                createdBy={chat.owner_id}
+                name={chat.name}
+                totalUsers={2}
+                enter={() => handleEnterChat(chat.id)}
+                join={() => handleJoinChat(chat.id)}
+                />
             ))}
-          </ul>
+          </div>
         ) : (
           <p>No chats available.</p>
         )}
@@ -56,4 +64,25 @@ const Chats: React.FC = () => {
 
 export default Chats;
 
+
+
+/*
+<div className="flex items-center">
+<span className="mr-2">{chat.name}</span>
+{user.user.id === chat.owner_id &&
+  <button
+    onClick={() => handleEnterChat(chat.id)}
+    className="bg-indigo-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+  >
+    Enter Chat
+  </button>
+}
+<button
+  onClick={() => handleJoinChat(chat.id)}
+  className="bg-indigo-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+>
+  Join Chat
+</button>
+</div>
+*/
 
