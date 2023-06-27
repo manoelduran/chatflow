b7a25a9414e93520f78997238dcf90558c693d4f
Manoel Duran <manoel.duran@hotmail.com>
2023-06-27 18:56:44 -0300

    feat(Input): created error verification

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,12 +1,12 @@
 import React, { InputHTMLAttributes, useEffect, useRef } from 'react'
-import {  useField } from '@unform/core'
+import { useField } from '@unform/core'
 
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
-    name: string;
-  }
+  name: string;
+}
 
- const Input: React.FC<InputProps> = ({name, onChange, ...rest}) => {
+const Input: React.FC<InputProps> = ({ name, onChange, ...rest }) => {
   const inputRef = useRef<HTMLInputElement>(null)
   const { fieldName, defaultValue, registerField, error } = useField(name)
 
@@ -24,9 +24,14 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
         ref.current.value = ''
       },
     })
-  }, [fieldName, registerField])
+  }, [fieldName, registerField, error])
+  return <>
+    <input ref={inputRef} className={error ? "w-full px-3 py-2 border border-red-700 rounded focus:outline-none focus:border-indigo-500" : "w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"} onChange={onChange} defaultValue={defaultValue} {...rest} />
 
-  return <input ref={inputRef}  className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500" onChange={onChange} defaultValue={defaultValue} {...rest} />
+    {error && (
+      <span className='text-sm text-red-700'>{error}</span>
+    )}
+  </>
 }
 
-export {Input}
+export { Input }
