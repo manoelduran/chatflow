322ab6ed908f41810a0a34fd976cee9addcfed77
Manoel Duran <manoel.duran@hotmail.com>
2023-06-27 19:03:23 -0300

    feat(create-user): created yup  validations to create-user

---
+++ src/pages/create-user/validations/index.ts
@@ -0,0 +1,13 @@
+import { SignUpDTO } from '@/src/dtos/user/SignUpDTO';
+import * as Yup from 'yup';
+
+export const canCreateUser = async (data: SignUpDTO): Promise<void> => {
+    const schema = Yup.object().shape({
+        username: Yup.string().required('username required!'),
+        email: Yup.string().email().required('email required!'),
+        password: Yup.string().min(6).required('Password with minimum of 6 characters is required!'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+}
---
+++ src/utils/getValidationErrors.ts
@@ -0,0 +1,16 @@
+import { ValidationError } from 'yup';
+
+interface Errors {
+  [key: string]: string;
+}
+
+export default function getValidationErrors(err: ValidationError): Errors {
+  const validationErrors: Errors = {};
+
+  err.inner.forEach(error => {
+    console.log('error 31231', error)
+    validationErrors[String(error.path)] = error.message;
+  });
+
+  return validationErrors;
+}
