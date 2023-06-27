5654725b3db22803dd1f51c28523edcd866055aa
Manoel Duran <manoel.duran@hotmail.com>
2023-06-27 19:03:00 -0300

    feat(create-user): created yup  validations

--- src/pages/create-user/index.tsx
+++ src/pages/create-user/index.tsx
@@ -1,33 +1,52 @@
 import AnimatedButton from '@/src/components/AnimatedButton';
+import * as Yup from 'yup';
+import { Input } from '@/src/components/Input';
 import { useAuth } from '@/src/contexts/auth';
+import { SignUpDTO } from '@/src/dtos/user/SignUpDTO';
+import { FormHandles } from '@unform/core';
+import { Form } from '@unform/web';
 import { useRouter } from 'next/router';
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
+import { canCreateUser } from './validations';
+import getValidationErrors from '@/src/utils/getValidationErrors';
 
 const CreateUser: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
   const router = useRouter();
   const {signUp} = useAuth();
   const [loading, setLoading] = useState(false);
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
 
-  const handleSignUp = useCallback(async (e: React.FormEvent) => {
+  const handleSignUp = useCallback(async (data: SignUpDTO) => {
+    setLoading(true)
     try{
-     setLoading(true)
-     e.preventDefault();
-     const parsedData =  { email, username, password }
-     await signUp(parsedData)
+      formRef.current?.setErrors([]);
+     await canCreateUser(data)
+     await signUp({email: data.email, password: data.password, username: data.username})
+       /*   addToast({
+          title: 'Ocorreu um erro',
+          type: 'error',
+          description: err.response.data.message,
+        });*/
      router.push('/chats');
-     setEmail('');
-     setUsername('');
-     setPassword('');
     } catch (error) {
      console.log('error', error)
-     setLoading(false)
+     if (error instanceof Yup.ValidationError) {
+      const errors = getValidationErrors(error);
+      console.log('errors', errors)
+      formRef.current?.setErrors(errors);
+
+      return;
+    }
+    /*   addToast({
+          title: 'Ocorreu um erro',
+          type: 'error',
+          description: err.response.data.message,
+        });*/
     } finally {
      setLoading(false)
+
     }
-   }, [email, username, password, loading]);
+   }, [loading ]);
 
   return (
     <div className="w-full  min-h-screen px-10 py-10 flex flex-col items-center bg-gray-100">
@@ -38,47 +57,32 @@ const CreateUser: React.FC = () => {
     <div className="flex items-center justify-center pt-20 bg-gray-100">
       <div className="bg-white shadow-md rounded-md px-8 pt-6 pb-8 mb-4">
         <h2 className="text-2xl font-bold mb-6">Create Account</h2>
-        <form className="space-y-4" onSubmit={handleSignUp}>
-          <div>
+        <Form ref={formRef} className="space-y-4" onSubmit={handleSignUp}>
+              <div>
             <label htmlFor="email" className="block font-bold mb-1">Email</label>
-            <input
-              type="email"
-              id="email"
-              className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+            <Input
+              name='email'
               placeholder="Enter your email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
             />
           </div>
           <div>
             <label htmlFor="username" className="block font-bold mb-1">Username</label>
-            <input
-              type="text"
-              id="username"
-              className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+            <Input
+              name='username'
               placeholder="Choose a username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              required
             />
           </div>
           <div>
             <label htmlFor="password" className="block font-bold mb-1">Password</label>
-            <input
-              type="password"
-              id="password"
-              className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-indigo-500"
+            <Input
+              name='password'
               placeholder="Choose a password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
             />
           </div>
           <div>
           <AnimatedButton type="submit" style="w-full bg-indigo-500 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" text=' Create' />
           </div>
-        </form>
+          </Form>
       </div>
     </div>
     </div>
