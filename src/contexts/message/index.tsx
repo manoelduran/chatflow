96b64b9ca1a0fb6c3ee504519ab062daa5d246b4
Manoel Cendon Duran <manoel.duran@hotmail.com>
2023-07-23 17:26:48 -0300

    feat(Messages): got message owner name

--- src/contexts/message/index.tsx
+++ src/contexts/message/index.tsx
@@ -6,41 +6,40 @@ import { CreateMessageDTO } from '@/src/dtos/message/CreateMessageDTO';
 import { DeleteMessageDTO } from '@/src/dtos/message/DeleteMessageDTO';
 import { EditMessageDTO } from '@/src/dtos/message/EditMessageDTO';
 import { FetchChatIdDTO } from '@/src/dtos/message/FetchChatIdDTO';
+import socket from '@/src/services/socket';
 
 interface MessageContextData {
-    messages?: MessageEntity[];
+    messages?: {message: MessageEntity, owner: string}[];
     chatId?: string | undefined;
     createMessage: (data: CreateMessageDTO) => Promise<void>;
     deleteMessage: (data: DeleteMessageDTO) => Promise<void>;
     editMessage: (data: EditMessageDTO) => Promise<void>;
-    listMessagesByChat: (data: MessagesByChatDTO) => Promise<MessageEntity[]>;
+    listMessagesByChat: (data: MessagesByChatDTO) => Promise<{message: MessageEntity, owner: string}[]>;
     fetchChatId: (data: FetchChatIdDTO) => Promise<void>;
 }
 
 const MessageContext = createContext<MessageContextData>({} as MessageContextData);
 
 const MessageProvider = ({children}: any) => {
-    const [messages, setMessages] = useState<MessageEntity[]>([] as MessageEntity[]);
+    const [messages, setMessages] = useState<{message: MessageEntity, owner: string}[]>([] as {message: MessageEntity, owner: string}[]);
     const [chatId, setChatId] = useState<string | undefined>(undefined);
 
     const messagesByChat = useCallback(async ({chat_id}: MessagesByChatDTO) => {
        if(!chat_id) {
   return []
        }
-       const response = await api.get<MessageEntity[]>(`/messages/${chat_id}`)
-       console.log("response da list", response.data)
+       const response = await api.get<{message: MessageEntity, owner: string}[]>(`/messages/${chat_id}`)
+       console.log('mensagens', response.data)
        setMessages(response.data);
        return response.data;
-    }, [messages, chatId]);
+    }, [messages, chatId, socket]);
     const handleFetchChatId = useCallback(async (data: FetchChatIdDTO) => {
         if(data.chat_id) {
             setChatId(data.chat_id);
         };
     }, [])
     const handleCreateMessage = useCallback(async (data: CreateMessageDTO) => {
-        console.log('data', data)
         const response = await api.post(`/messages/${data.chat_id}`, {text: data.text}, {headers: {Authorization: `Baerer ${data.token}`}})
-        console.log('response do create', response)
         if(response.status === 201) {
             messagesByChat({chat_id: data.chat_id})
         }
--- src/pages/chat/[id]/index.tsx
+++ src/pages/chat/[id]/index.tsx
@@ -17,6 +17,7 @@ import { ParsedUrlQuery } from 'querystring';
 import React, { useState, useRef, useEffect, useCallback, useMemo } from 'react';
 import { canCreateMessage } from './validations';
 import getValidationErrors from '@/src/utils/getValidationErrors';
+import { useRouter } from 'next/router';
 
 type ChatProps = {
   chat: ChatEntity;
@@ -27,10 +28,10 @@ interface FormCredentials {
 }
 const Chat: NextPage<ChatProps> = ({ chat }): JSX.Element => {
   const formRef = useRef<FormHandles>(null);
-  const { user } = useAuth()
-
+  const { user, signOut } = useAuth()
+  const router = useRouter();
   const { messages, createMessage, listMessagesByChat } = useMessage()
-  const { socket } = useSocket();
+  const { socket, message } = useSocket();
   const [sending, setSending] = useState(false);
 
   const handleSendMessage = useCallback(async (data: FormCredentials) => {
@@ -38,16 +39,16 @@ const Chat: NextPage<ChatProps> = ({ chat }): JSX.Element => {
     try {
       formRef.current?.setErrors([]);
       await canCreateMessage(data)
-      console.log('user', user)
+
       const parsedData = {
         text: data.text,
         chat_id: chat.id,
         token: user.token
       } as CreateMessageDTO
       console.log(parsedData)
-  console.log('parsedData', parsedData)
+
       await createMessage(parsedData)
-      socket?.emit("message", { parsedData })
+      message(parsedData)
     } catch (error) {
       console.log('error', error)
       if (error instanceof Yup.ValidationError) {
@@ -56,6 +57,10 @@ const Chat: NextPage<ChatProps> = ({ chat }): JSX.Element => {
         formRef.current?.setErrors(errors);
 
         return;
+      }
+      if(error.response.status === 401) {
+        signOut();
+        router.push(`/`);
       }
       setSending(false)
     } finally {
@@ -66,22 +71,49 @@ const Chat: NextPage<ChatProps> = ({ chat }): JSX.Element => {
     if (!messages) {
       return []
     }
-    return messages?.value?.map((message: MessageEntity) => {
-      console.log('user 444', user)
+    return messages?.value?.map(({message, owner}: {message: MessageEntity, owner: string}) => {
+   
       return {
         ...message,
         myMessage: message.userId === user?.user?.id && message.text,
+        owner: owner,
         othersMessages: message.userId !== user?.user?.id && message.text,
         formattedSendAt: format(new Date(message?.sentAt), 'HH:mm')
       }
     })
   }, [messages, user])
+  
+
+
   useEffect(() => {
-    if(chat) {
+    if(chat ) {
       listMessagesByChat({chat_id: chat.id as string})
     }
   }, [chat]);
 
+  useEffect(() => {
+    socket?.on("sent", (arg) => {
+      console.log("arg da msg", arg)
+      listMessagesByChat({chat_id: chat.id as string})
+  })
+  }, [])
+useEffect(() => {
+  if (typeof window !== 'undefined' && list !== undefined) {
+    
+    const messageContainer = document?.getElementById('messageContainer')
+    const scrollToBottom =  () => {
+      console.log('containerMessage', messageContainer)
+      messageContainer?.scrollTo({
+       top: messageContainer?.scrollHeight,
+       behavior: 'smooth',
+     });
+   };
+    console.log('messageContainer dentro', messageContainer)
+  
+    scrollToBottom()
+  }
+
+}, [list])
 
   console.log('list', list)
   return (
@@ -92,22 +124,23 @@ const Chat: NextPage<ChatProps> = ({ chat }): JSX.Element => {
       <div className="w-full overflow-y-auto bg-white shadow-md rounded px-8 py-6 mb-8">
         <h2 className="text-2xl font-bold mb-6">{chat.name}</h2>
         <div
+        id='messageContainer'
           className="w-full max-h-64 overflow-y-auto bg-gray-200 p-4 mb-4"
         >
-          {list && user && list?.map((message, index) => (
+          {list && user && list?.map((message , index) => (
             <>
               {message.userId === user?.user?.id ? (
-                <div key={message.id} className="w-full h-fit  align-middle flex justify-end  mb-4">
+                <div id='messageContainer'  key={message.id} className="w-full h-fit  align-middle flex justify-end  mb-4">
                   <div className="w-fit flex  flex-col  bg-green-200 align-middle rounded-lg py-2 px-4 text-gray-800">
-                    <p  >{user?.user?.username}</p>
+                    <p  >{message.owner}</p>
                     <p  >{message.myMessage}</p>
                     <p className="text-end">{message.formattedSendAt}</p>
                   </div>
                 </div>
               ) : (
-                <div key={message.id} className="w-fit flex flex-col justify-start  mb-4">
+                <div id='messageContainer' key={message.id} className="w-fit flex flex-col justify-start  mb-4">
                   <div className="bg-red-200  rounded-lg py-2 px-4 text-gray-800 mb-1">
-                  <p  >{user?.user?.username}</p>
+                  <p  >{message.owner}</p>
                     <p >{message.othersMessages}</p>
                     <p className="text-end">{message.formattedSendAt}</p>
                   </div>
