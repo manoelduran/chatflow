9b564c51f013f47ab5c62fef1b1a40c3dbe9298b
Manoel Duran <manoel.duran@hotmail.com>
2023-08-25 23:37:33 -0300

    https to socket

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -3,7 +3,8 @@ import socketio from 'socket.io-client';
 const socket = socketio(`${process.env.NEXT_PUBLIC_API_URL}`, {
     autoConnect: false,
     transports: ['websocket'],
-    secure: false,
+    
+    secure: true,
     path: '/apiws/socket.io',
     rejectUnauthorized: false
   });
